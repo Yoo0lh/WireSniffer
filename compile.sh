dcae2cc9a120d35e9d7f64aefe20c4606ebf3116
Hamza Lahrach <hlahrach@e2r8p1.1337.ma>
2023-07-05 17:05:42 +0100

    remove the -werror flag and add fzanitize to compelation

--- compile.sh
+++ compile.sh
@@ -1,13 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
 if [[ $1 == "run" ]]
-then 
-  gcc -Wall -Wextra -Werror main.c -o wiresniffer
-  ./wiresniffer  
+then
+  gcc -Wall -Wextra main.c -o wiresniffer -fsanitize=address -g
+  ./wiresniffer
   rm -f wiresniffer
 elif [[ $1 == "clean" ]]
 then
-  rm -f wiresniffer; 
+  rm -f wiresniffer;
+  rm -f log_file.txt;
 fi
 
-:
